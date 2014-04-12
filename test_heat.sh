dae6790d90077ebab2f485652b1a511a94234883
admin <archii>
2014-04-12 08:20:59 +0000

    fixed random stack name generation issue by prepending uuid-y string with an x; fixed some variable usage issues

--- test_heat.sh
+++ test_heat.sh
@@ -1,11 +1,11 @@
-STACK_NAME=$1
+STACK_NAME=x$1
 #STACK_TEMPLATE=$2
 
 if [[ -z $STACK_NAME ]]; then
     exit 1;
 fi
 
-heat stack-create -f heat/ponytest_stack.yml $1
+heat stack-create -f heat/ponytest_stack.yml $STACK_NAME
 
 function getStackStatus () {
     heat stack-show $STACK_NAME | grep "stack_status " | awk -F"|" '{print $3}' | sed -e 's/^ //'
