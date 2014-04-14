8b7d1bd25eab4e1763202c7c268ea5687e96f760
admin <archii>
2014-04-14 03:56:43 +0000

    test_heat.sh now pushes parameters to heat for the ponytest_stack.yml template

--- test_heat.sh
+++ test_heat.sh
@@ -1,11 +1,26 @@
-STACK_NAME=x$1
-#STACK_TEMPLATE=$2
+GEN_NAME=$1
+STACK_NAME=x${GEN_NAME}
+HOST_NAME=h${GEN_NAME}
+STACK_TEMPLATE=$2
+IMAGE_NAME=$3
 
-if [[ -z $STACK_NAME ]]; then
+USAGE="\n    Usage:\n    $0 BASE_NAME STACK_TEMPLATE [IMAGE_NAME]\n\n    BASE_NAME and STACK_TEMPLATE are required.\n"
+
+if [[ -z $GEN_NAME ]]; then
+    echo -e $USAGE;
     exit 1;
 fi
 
-heat stack-create -f heat/ponytest_stack.yml $STACK_NAME
+if [[ -z $STACK_TEMPLATE ]]; then
+    echo -e $USAGE;
+    exit 1;
+fi
+
+if [[ -z $IMAGE_NAME ]]; then
+    IMAGE_NAME='';
+fi
+
+heat stack-create -f $STACK_TEMPLATE $STACK_NAME -P myhostname=${HOST_NAME};myimagename=${IMAGE_NAME}
 
 function getStackStatus () {
     heat stack-show $STACK_NAME | grep "stack_status " | awk -F"|" '{print $3}' | sed -e 's/^ //'
