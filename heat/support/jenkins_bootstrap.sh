607ae8e572b8023113d186e764ea53f1e611df97
Archie Harris <archie.harris@RACKSPACE.COM>
2014-04-16 14:05:56 +1000

    Need the -y flag for automated yum install

--- heat/support/jenkins_bootstrap.sh
+++ heat/support/jenkins_bootstrap.sh
@@ -2,4 +2,4 @@ yum -y -v install java-1.8.0-openjdk-headless.x86_64 git wget
 pip install python-heatclient
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key
-yum install jenkins
+yum -y -v install jenkins
