4439651f6fe7e8d5b1e81f4346074f3d149ffe51
Archie Harris <archie.harris@RACKSPACE.COM>
2014-04-16 15:10:20 +1000

    refining java install for jenkins use

--- heat/support/jenkins_bootstrap.sh
+++ heat/support/jenkins_bootstrap.sh
@@ -1,4 +1,4 @@
-yum -y -v install java-1.8.0-openjdk-headless.x86_64 git wget;
+yum -y -v install java git wget;
 pip install python-heatclient;
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo && \
   rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key && \
