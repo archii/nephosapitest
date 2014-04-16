db402f30b5e5ce7644e992422a5fd7690e79d6ee
Archie Harris <archie.harris@RACKSPACE.COM>
2014-04-16 14:14:26 +1000

    making jenkins install and service enabling nicer

--- heat/support/jenkins_bootstrap.sh
+++ heat/support/jenkins_bootstrap.sh
@@ -1,5 +1,8 @@
-yum -y -v install java-1.8.0-openjdk-headless.x86_64 git wget
-pip install python-heatclient
-wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key
-yum -y -v install jenkins
+yum -y -v install java-1.8.0-openjdk-headless.x86_64 git wget;
+pip install python-heatclient;
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo && \
+  rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key && \
+  yum -y -v install jenkins && \
+  systemctl start jenkins && \
+  systemctl status jenkins && \
+  systemctl enable jenkins;
