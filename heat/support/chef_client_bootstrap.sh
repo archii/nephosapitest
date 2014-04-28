917282e265bc3f74e1566df4bac19d9af59443bb
Archie Harris <archie.harris@RACKSPACE.COM>
2014-04-28 16:05:48 +1000

    initial chef client Heat template and bootstrap glue

---
+++ heat/support/chef_client_bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source /opt/raxapitest/chef_bootstrap.env;
+echo $PLATFORM
+echo $PLATFORM_VERSION
+echo $MACHINE_ARCH
+
+sleep 60;
+
+yum -y -v remove firewalld && \
+  yum -y -v install iptables-services && \
+  sleep 30 && \
+  service iptables restart;
+
+systemctl enable iptables.service;
+
+yum -y -v update;
+
+userdel -rf fedora;
+
+#yum install -y -v https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chef-11.12.2-1.el6.x86_64.rpm && \
+yum install -y -v http://www.opscode.com/chef/download?p=$PLATFORM&pv=$PLATFORM_VERSION&m=$MACHINE_ARCH&v=latest&prerelease=false&nightlies=false && \
+  exit 0
