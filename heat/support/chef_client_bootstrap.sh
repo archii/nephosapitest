bda7945cfc5c0bbc70f75d385e70127df448637b
Archie Harris <archie.harris@RACKSPACE.COM>
2014-04-29 16:57:44 +1000

    cleaning up bootstrap script;  adding /etc/chef/client.rb for tcserious;

--- heat/support/chef_client_bootstrap.sh
+++ heat/support/chef_client_bootstrap.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-source /opt/raxapitest/chef_client_bootstrap.env;
-echo $PLATFORM;
-echo $PLATFORM_VERSION;
-echo $MACHINE_ARCH;
-
 sleep 60;
 
 yum -y -v remove firewalld && \
@@ -18,7 +13,5 @@ yum -y -v update;
 
 userdel -rf fedora;
 
-#yum install -y -v https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chef-11.12.2-1.el6.x86_64.rpm && \
-#yum install -y -v http://www.opscode.com/chef/download?p=$PLATFORM&pv=$PLATFORM_VERSION&m=$MACHINE_ARCH&v=latest&prerelease=false&nightlies=false && \
 curl -L https://www.opscode.com/chef/install.sh | bash && \
   exit 0
---
+++ heat/support/etc/chef/client.rb
@@ -0,0 +1,4 @@
+log_location     STDOUT
+chef_server_url  "https://api.opscode.com/organizations/tcserious"
+validation_client_name "tcserious-validator"
+# Using default node name (fqdn)
