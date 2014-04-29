917e34e45b69e0abb890d70b1e663e29294ea6cd
Archie Harris <archie.harris@RACKSPACE.COM>
2014-04-29 17:25:07 +1000

    cleaned up bootstrap script for chef client instance provisioning;  cleaned up template for chef client instance provisioning

--- heat/support/chef_client_bootstrap.sh
+++ heat/support/chef_client_bootstrap.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-sleep 60;
+sleep 20;
 
-yum -y -v remove firewalld && \
-  yum -y -v install iptables-services && \
-  sleep 30 && \
-  service iptables restart;
-
-systemctl enable iptables.service;
-
-yum -y -v update;
-
-userdel -rf fedora;
+#yum -y -v remove firewalld && \
+#  yum -y -v install iptables-services && \
+#  sleep 10 && \
+#  service iptables restart;
+#
+#systemctl enable iptables.service;
+#
+#yum -y -v update;
+#
+#userdel -rf fedora;
 
 curl -L https://www.opscode.com/chef/install.sh | bash && \
   exit 0
