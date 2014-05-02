ac65de5d60d205838a0b6711099757e0d9c7caf2
Archie Harris <archie.harris@RACKSPACE.COM>
2014-05-02 10:19:26 +1000

    removing iptables hackery - Chef will manage this going forward; removing deprecated files, parameters and test/sample code; removing deprecated eggs687-201404130220 cloud image file; substituting cloud files URL for personality mapped Chef attribute JSON file initialization

--- heat/support/7424base_bootstrap.sh
+++ heat/support/7424base_bootstrap.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 sleep 30;
-yum -y -v remove firewalld && \
-  yum -y -v install iptables-services && \
-  sleep 10 && \
-  service iptables restart;
-yum -y -v update && \
-  systemctl enable iptables.service && \
-  exit 0;
+yum -y -v update; 
